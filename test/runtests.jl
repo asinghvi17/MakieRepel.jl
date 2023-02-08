7e30c4885d6c3249542921079c5b730d36f55696
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-02-08 20:12:33 +0530

    mtcars kind of works

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,7 +12,7 @@ using BenchmarkTools, TimerOutputs
     # Write your tests here.
 end
 
-
+# Basic test
 boxes = Rect2f.(randn(Point2f, 40), Point2f.(rand(40) .* 5, rand(40))./ 4)
 
 TimerOutputs.reset_timer!(to)
@@ -21,18 +21,21 @@ to
 
 
 f, a1, p1 = poly(boxes; color = 1:length(boxes), colormap = cgrad(:rainbow1; alpha = 0.4))
-a2, p2 = poly(f[1, 2], repel_from_points(origin.(boxes), boxes, 3000; padding = 0.05); color = 1:length(boxes), colormap = cgrad(:rainbow1; alpha = 0.4))
+newpoints = repel_from_points(origin.(boxes), boxes, 3000; padding = 0.05)
+a2, p2 = poly(f[1, 2], Rect2f.(newpoints, widths.(boxes)); color = 1:length(boxes), colormap = cgrad(:rainbow1; alpha = 0.4))
+linesegments!(a2, collect(Iterators.flatten(zip(origin.(boxes), newpoints))))
 scatter!(a1, origin.(boxes); markersize = 5, color = :black)
 scatter!(a2, origin.(boxes); markersize = 5, color = :black)
 linkaxes!(a1, a2)
-title = Label(f[0, 1:2], text = "Iteration 1")
+title = Label(f[0, 1:2], text = "3,000 iterations")
 f
 
 
 @time record(f, "repel.mp4", 1:10:5000) do i
     global p2
     delete!(a2.scene, p2)
-    p2 = poly!(a2, repel_from_points(origin.(boxes), boxes, i; padding = 0.05); color = 1:length(boxes), colormap = cgrad(:rainbow1; alpha = 0.4))
+    newpoints = repel_from_points(origin.(boxes), boxes, i; padding = 0.05)
+    p2 = poly!(a2, Rect2f.(newpoints, widths.(boxes)); color = 1:length(boxes), colormap = cgrad(:rainbow1; alpha = 0.4))
     autolimits!(a2)
     title.text = "Iteration $i"
 end
@@ -40,7 +43,8 @@ end
 @time record(f, "repel_x.mp4", 1:10:5000) do i
     global p2
     delete!(a2.scene, p2)
-    p2 = poly!(a2, repel_from_points(origin.(boxes), boxes, i; padding = 0.05, x = true, y = false); color = 1:length(boxes), colormap = cgrad(:rainbow1; alpha = 0.4))
+    newpoints = repel_from_points(origin.(boxes), boxes, i; padding = 0.05, x = true, y = false)
+    p2 = poly!(a2, Rect2f.(newpoints, widths.(boxes)); color = 1:length(boxes), colormap = cgrad(:rainbow1; alpha = 0.4))
     autolimits!(a2)
     title.text = "Iteration $i"
 end
@@ -48,7 +52,8 @@ end
 @time record(f, "repel_y.mp4", 1:10:5000) do i
     global p2
     delete!(a2.scene, p2)
-    p2 = poly!(a2, repel_from_points(origin.(boxes), boxes, i; padding = 0.05, x = false, y = true); color = 1:length(boxes), colormap = cgrad(:rainbow1; alpha = 0.4))
+    newpoints = repel_from_points(origin.(boxes), boxes, i; padding = 0.05, x = false, y = true)
+    p2 = poly!(a2, Rect2f.(newpoints, widths.(boxes)); color = 1:length(boxes), colormap = cgrad(:rainbow1; alpha = 0.4))
     autolimits!(a2)
     title.text = "Iteration $i"
 end
@@ -60,7 +65,6 @@ mtpoints = Point2f.(mtcars.WT, mtcars.MPG)
 ax, sc = scatter(fig[1, 1], mtpoints)
 
 tp = text!(ax, mtcars.Model; position = mtpoints)
-
 fig
 
 pixel_mtpoints = Point2f.(Makie.project.((Makie.camera(ax.scene),), :data, :pixel, mtpoints))# .- (origin(ax.scene.px_area[]),)
@@ -69,12 +73,16 @@ boxes = Makie.boundingbox.(tp.plots[1].plots[1][1][], Makie.to_ndim.(Point3f, tp
 
 repellable_boxes = Rect2f.(origin.(boxes) .+ pixel_mtpoints, widths.(boxes))
 
-scatter!(ax, pixel_mtpoints; space=:pixel, color = :red)
+# scatter!(ax, pixel_mtpoints; space=:pixel, color = :red)
 
+tp.position[] = new_boxes
+tp.space = :pixel
 
 fig
 
-poly!(ax.scene, repellable_boxes; space = :pixel, color = (:black, 0.1))
+pr = poly!(ax.scene, repellable_boxes; space = :pixel, color = (:black, 0.1))
+
+translate!(pr, 0, -20, 0)
 
 fig
 
@@ -87,10 +95,10 @@ to
 
 poly(repellable_boxes; axis = (aspect = DataAspect(),))
 
-poly!(ax, new_boxes; color = (:black, 0.3))
+poly(Rect2f.(new_boxes, widths.(repellable_boxes)); color = (:black, 0.3))
 fig
 
-new_boxes = repel_from_points(mtpoints, repellable_boxes, 5000; padding = 4)
+new_boxes = repel_from_points(pixel_mtpoints, repellable_boxes, 5000; padding = 4)
 
 f2 = Figure()
 a1, p1 = poly(f2[1, 1], repellable_boxes; color = 1:length(repellable_boxes), colormap = cgrad(:rainbow, alpha=0.2))
