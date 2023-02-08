e0371df91ec75a608db2f82a9883443c0cc44d7e
Documenter.jl <documenter@juliadocs.github.io>
2023-02-08 14:12:05 +0000

    build based on 5e86061

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MakieRepel","category":"page"},{"location":"#MakieRepel","page":"Home","title":"MakieRepel","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MakieRepel.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MakieRepel]","category":"page"}]
+}
